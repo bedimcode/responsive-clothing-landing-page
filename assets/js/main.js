8305e4e69f58f65a57d641edef0895cc98d2fbab
Marlon <61248995+bedimcode@users.noreply.github.com>
2022-06-18 10:26:06 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
